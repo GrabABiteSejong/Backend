f7ff2da8b4b236da97f8d37a3731ee83f55ae650
lulyulalla <daye200@naver.com>
2025-12-23 11:21:45 +0900

    Add files via upload

---
+++ PleaseMajor/src/main/java/project/pleasemajor/PleaseMajorApplication.java
@@ -0,0 +1,13 @@
+package project.pleasemajor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PleaseMajorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PleaseMajorApplication.class, args);
+  }
+
+}
